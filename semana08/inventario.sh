00a5bc13f2dcdf2547193761ee0ed944088573f7
Raul Fernando Condo Choque <raul.condo@sistemas.edu.bo>
2026-04-12 23:39:37 -0400

    feat: add mapfile to load file list into indexed array

--- semana08/inventario.sh
+++ semana08/inventario.sh
@@ -9,3 +9,6 @@ exit 1
 fi
 
 echo "Analizando repositorio: $REPO"
+# --- 1. Cargar lista de archivos ---
+mapfile -t archivos < <(find "$REPO" -type f | sort)
+echo "Total de archivos encontrados: ${# archivos[@]}"
