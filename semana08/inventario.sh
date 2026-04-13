9b936e1ee2b13c564082967f84440671f342cf24
Raul Fernando Condo Choque <raul.condo@sistemas.edu.bo>
2026-04-12 23:44:36 -0400

    feat: build weekly summary matrix (scripts , docs , size)

--- semana08/inventario.sh
+++ semana08/inventario.sh
@@ -38,3 +38,17 @@ else
 tiene_readme["$nombre"]="NO"
 fi
 done
+# --- 4. Matriz de resumen por semana ---
+mapfile -t semanas < <(ls -d "$REPO"/semana*/ 2>/dev/null | sort)
+COLS=3 # scripts , docs , tamano_kb
+declare -a matriz_sem
+
+for (( i=0; i<${#semanas[@]}; i++ )); do
+dir="${semanas[$i]}"
+scripts=$(find "$dir" -name "*.sh" | wc -l)
+docs=$(find "$dir" -name "*.md" | wc -l)
+kb=$(du -sk "$dir" 2>/dev/null | awk '{print $1}')
+matriz_sem[$(( i*COLS+0 ))]=$scripts
+matriz_sem[$(( i*COLS+1 ))]=$docs
+matriz_sem[$(( i*COLS+2 ))]=${kb:-0}
+done
